84885de837dacae8e3fc96dea492e4f8d17fcc46
Katrina_Dierking <kalynndier@gmail.com>
2019-11-06 18:13:05 -0500

    dark mode not working. Working on bugs

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,15 +1,22 @@
 import {useLocalStorage} from './useLocalStorage';
 import {useEffect} from 'react';
 
-export const useDarkMode = (dark) => {
+const useDarkMode = (dark) => {
 
-    const [darkMode, setDarkMode] = useLocalStorage (dark);
+    const [darkMode, setDarkMode] = useLocalStorage ('dark');
 
     useEffect (() => {
-        
-    })
+       if (darkMode === true) {
+       document.body.classList.add ('dark-mode');
+    }
+        else {
+            document.body.classList.remove ('dark-mode')
+        }
+    
+    }, [darkMode])
 
 
     return [darkMode, setDarkMode];
 }
 
+export default useDarkMode;
